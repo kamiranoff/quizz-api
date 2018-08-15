86760bbb3905e27c50ee98b4251dc262267eb2c1
Kevin Amiranoff <kamiranoff@gmail.com>
2018-08-15 22:17:39 +0100

    fix typing

--- src/questions/questions.service.ts
+++ src/questions/questions.service.ts
@@ -1,19 +1,24 @@
-import { Model } from 'mongoose';
+import Mongoose, { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { IQuestion } from './interfaces/question.interface';
 import { CreateQuestionDto } from './create-question.dto';
 
+
+export interface IQuestionModel extends Mongoose.Document, IQuestion {
+}
+
 @Injectable()
 export class QuestionsService {
-  constructor(@InjectModel('Question') private readonly questionModel: Model<IQuestion>) {}
+  constructor(@InjectModel('Question') private readonly questionModel: Model<IQuestionModel>) {
+  }
 
-  async create(createQuestionDto: CreateQuestionDto): Promise<IQuestion> {
+  async create(createQuestionDto: CreateQuestionDto): Promise<IQuestionModel> {
     const createdQuestion = new this.questionModel(createQuestionDto);
     return await createdQuestion.save();
   }
 
-  async findAll(): Promise<IQuestion[]> {
+  async findAll(): Promise<IQuestionModel[]> {
     return await this.questionModel.find().exec();
   }
 }
